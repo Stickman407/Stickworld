b13afa3beaccb06469f5405ca7d9eeaf499695bf
Stick Man <stickman4072@gmail.com>
2021-12-07 17:41:38 -0600

    Changed from Object to Class

--- scripts.js
+++ scripts.js
@@ -1,35 +1,39 @@
+class Tank {
+    el = null;
+    x = 0;
+    y = 0;
+    orientation = 0;
+    constructor() {
+        this.el =
+          document.createElement ("span");
+        this.el.style.position =
+          "absolute";
+        this.el.style.transition = "0.3s linear all";
+        this.el.style.background = "url('https://www.kindpng.com/picc/m/759-7593655_tank-commando-2-shape-commando-2-tank-sprites.png') center center / contain no-repeat";
+      this.el.style.height = "60px";
+      this.el.style.width = "60px";
+      document.body.appendChild(this.el);
+       return this.el;
+    }
+
+    move (x, y) {
+        this.x = this.x + x;
+        this.y = this.y + y;
+        this.el.style.left = `${this.x}px`;
+        this.el.style.top = `${this.y}px`;
+        return this.el;
+      }
+        setOrientation (degrees) {
+          this.orientation = degrees;
+          this.el.style.transform =
+            `rotate (${degrees}deg)`;
+          return this.el;
+        }
+}
+
+
+
 const movementSpeed = 15;
-const tank = {
-  el: null,
-  x: 0,
-  y: 0,
-  orientation: 0,
-  create: function() {
-    this.el =
-      document.createElement ("span");
-    this.el.style.position =
-      "absolute";
-    this.el.style.transition = "0.3s linear all";
-    this.el.style.background = "url('https://www.kindpng.com/picc/m/759-7593655_tank-commando-2-shape-commando-2-tank-sprites.png') center center / contain no-repeat";
-  this.el.style.height = "60px";
-  this.el.style.width = "60px";
-  document.body.appendChild(this.el);
-   return this.el;
-},
-move: function(x,y) {
-  this.x = this.x + x;
-  this.y = this.y + y;
-  this.el.style.left = `${this.x}px`;
-  this.el.style.top = `${this.y}px`;
-  return this.el;
-},
-  setOrientation: function(degrees) {
-    this.orientation = degrees;
-    this.el.style.transform =
-      `rotate (${degrees}deg)`;
-    return this.el;
-  }
-};
 document.addEventListener("keydown",(event) => {
   if (event.key === "ArrowUp") {
     tank.setOrientation(0);
@@ -49,5 +53,6 @@ movementSpeed);
   tank.move(movementSpeed, 0);
 }
                           });
-const chris = tank.create();
+
+const chris = new Tank();
 
